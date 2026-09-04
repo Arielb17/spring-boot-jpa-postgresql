d9886c275c431cc3b1e19af3053ae0afea6a8e50
Ariel Batista <138503250+Arielb17@users.noreply.github.com>
2026-09-04 09:19:19 -0300

    fix: torna busca exata por título case-insensitive

--- src/main/java/com/bezkoder/spring_boot_jpa_postgresql/repository/TutorialRepository.java
+++ src/main/java/com/bezkoder/spring_boot_jpa_postgresql/repository/TutorialRepository.java
@@ -4,11 +4,14 @@ import com.bezkoder.spring_boot_jpa_postgresql.model.Tutorial;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
     List<Tutorial> findByPublished(boolean published);
 
-    List<Tutorial> findByTitle(String title);
+    @Query("SELECT t FROM Tutorial t WHERE LOWER(t.title) = LOWER(:title)")
+    List<Tutorial> findByTitle(@Param("title") String title);
 
     List<Tutorial> findByTitleContaining(String title);
 }
